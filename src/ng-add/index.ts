3fa774757fd4ea66921344a7140c89eae2191529
nontangent <irregular0number@gmail.com>
2021-01-06 23:20:13 +0900

    version 10

--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -1,32 +1,38 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { NodePackageInstallTask, RunSchematicTask } from '@angular-devkit/schematics/tasks';
 import { 
-	addPackageToPackageJson,
+  addPackageToPackageJson,
+  getPackageVersionFromPackageJson
 } from '../utilities';
 
 export default function(options: any): Rule {
   return (host: Tree, context: SchematicContext) => {
-		const packageName = 'angular-atomic-schematics';
+    const ngCoreVersionTag = getPackageVersionFromPackageJson(host, '@angular/core');
+    const angularDependencyVersion = ngCoreVersionTag || `0.0.0-NG`;
+
+    if (angularDependencyVersion === '0.0.0-NG') {
+      throw new Error('@angular/core version is not supported.');
+    }
 
 		// 必要なパッケージの追加
 		addPackageToPackageJson(
 			host, 
-			packageName, 
-			'0.1000.x',
+			'angular-atomic-schematics', 
+			getCustomWebpackVersion(angularDependencyVersion),
 			'devDependencies'
 		);
 
 		addPackageToPackageJson(
 			host,
 			'angular-host-css-variable',
-			'0.2.x',
+			getCustomWebpackVersion(angularDependencyVersion),
 			'devDependencies'
 		);
 
 		addPackageToPackageJson(
 			host,
 			'angular-custom-webpack-chaining',
-			'0.1000.x',
+      getCustomWebpackVersion(angularDependencyVersion),
 			'devDependencies'
 		);
 
@@ -43,5 +49,14 @@ export default function(options: any): Rule {
 		context.addTask(new RunSchematicTask('setup-project', {...options}), [runSchematicTask])
 
 		return host
+  }
+}
+
+export function getCustomWebpackVersion(ver: string): string {
+  const [major, minor, patch] = (ver.replace(/(\^|\~)/, '')).split('.');
+  switch (major) {
+    case '10': return '^0.1000.0';
+    case '11': return '^0.1100.0';
+    default: throw new Error('@angular/core version is not supported.');
   }
 }
